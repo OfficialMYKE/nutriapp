256068afc84695060a6615d3e974fabd78937ec5
OfficialMYKE <michaelcarrillo.ec@gmail.com>
2026-01-29 00:37:36 -0500

    configuracion next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // permitir imágenes de cualquier dominio (Unsplash, etc.)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
